ad2f8e1c894c242ea9cb04538ec1a65c0d1c3f26
Economiza Sul <economizasul@gemail.com>
2025-10-31 15:46:12 -0300

    fix: adicionada dependência ausente get-stream

--- routes/reports.js
+++ routes/reports.js
@@ -2,7 +2,6 @@
 const express = require('express');
 const router = express.Router();
 const { pool } = require('../config/db');
-const db = require('../src/db');
 const ReportController = require('../controllers/ReportController');
 const { protect, admin } = require('../middleware/authMiddleware'); // Importar middlewares
 
