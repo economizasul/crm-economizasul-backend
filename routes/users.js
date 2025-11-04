0c62f7ad0bf8e71ef784bfd99ec0f4893960c24a
Economiza Sul <economizasul@gemail.com>
2025-11-03 23:22:53 -0300

    Commit correção de rotas arquivo user.js

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,7 @@
 const express = require('express');
 const router = express.Router();
 
-const UserController = require('../../controllers/UserController'); 
+const UserController = require('../controllers/UserController'); 
 
 const isAuthenticated = require('../middlewares/authMiddleware'); // O nome do seu arquivo é authMiddleware.js ou isAuthenticated.js? Usando o nome mais provável
 const isAdministrator = require('../middlewares/isAdministrator'); 
