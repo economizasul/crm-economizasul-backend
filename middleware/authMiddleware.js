c19b23813b63355ea6b20fad780bd4f4f0f841ce
Economiza Sul <economizasul@gemail.com>
2025-12-01 19:19:26 -0300

    Fix: correção do log de erro do deploy para Correções para o novo relatório motivos de perdas

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,4 +1,4 @@
-// src/middleware/authMiddleware.js
+// middleware/authMiddleware.js
 
 const jwt = require('jsonwebtoken');
 const pool = require('../db');
--- routes/reportRoutes.js
+++ routes/reportRoutes.js
@@ -3,6 +3,7 @@ const express = require('express');
 const router = express.Router();
 const { protect } = require('../middleware/authMiddleware');
 const ReportController = require('../controllers/ReportController');
+const authMiddleware = require('../middleware/authMiddleware');
 
 router.use(protect);
 
